15d56584694d4e4a6e3209f28d9bd3885d4a014c
Carl Johnson <ftsys1992@gmail.com>
2024-05-21 11:28:39 +0000

    add CalculateTaskDates tests

--- tests/Feature/Jobs/CalculateTaskDatesTest.php
+++ tests/Feature/Jobs/CalculateTaskDatesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Jobs;
 
+use App\Jobs\CalculateTaskDates;
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,22 +12,67 @@ class CalculateTaskDatesTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_calculates_start_and_end_dates_for_a_task(): void
+    public function test_calculates_start_date_and_end_date_for_a_task(): void
     {
-        $user = User::factory()->create();
-        $task1 = Task::factory()->create([
+        $user = User::factory()->create([
+            'working_hours_start' => '09:00',
+            'working_hours_end' => '17:00',
+        ]);
+
+        Task::factory()->create([
             'assignee_id' => $user->id,
-            'start_at' => '2024-04-30 12:30:00',
-            'end_at' => '2024-04-30 13:30:00'
+            'status' => 'confirmed',
+            'duration' => 'PT1H',
+            'start_at' => '2024-04-30 09:00:47',
+            'end_at' => '2024-04-30 10:00:47'
         ]);
 
         $task2 = Task::factory()->create([
             'assignee_id' => $user->id,
-            'start_at' => '2024-04-30 14:00:47',
-            'end_at' => '2024-05-01 12:30:47'
+            'status' => 'confirmed',
+            'duration' => 'PT2H30M',
         ]);
 
-        self::assertEquals($user->id, $task1->assignee_id);
+        $job = new CalculateTaskDates($task2->id);
+
+        $job->handle();
+
+        $task2 = $task2->fresh();
+
         self::assertEquals($user->id, $task2->assignee_id);
+        self::assertEquals('2024-04-30 10:00:47', $task2->start_at);
+        self::assertEquals('2024-04-30 12:30:47', $task2->end_at);
+    }
+
+    public function test_calculates_start_date_and_next_day_end_date_for_a_task(): void
+    {
+        $user = User::factory()->create([
+            'working_hours_start' => '09:00',
+            'working_hours_end' => '17:00',
+        ]);
+
+        Task::factory()->create([
+            'assignee_id' => $user->id,
+            'status' => 'confirmed',
+            'duration' => 'PT1H',
+            'start_at' => '2024-04-30 09:00:47',
+            'end_at' => '2024-04-30 10:00:47'
+        ]);
+
+        $task2 = Task::factory()->create([
+            'assignee_id' => $user->id,
+            'status' => 'confirmed',
+            'duration' => 'PT8H30M',
+        ]);
+
+        $job = new CalculateTaskDates($task2->id);
+
+        $job->handle();
+
+        $task2 = $task2->fresh();
+
+        self::assertEquals($user->id, $task2->assignee_id);
+        self::assertEquals('2024-04-30 10:00:47', $task2->start_at);
+        self::assertEquals('2024-05-01 10:30:47', $task2->end_at);
     }
 }
